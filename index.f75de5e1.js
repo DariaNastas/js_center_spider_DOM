fcb77fbc361bdcc5707fbc3565939273261ba805
Daria Nastas <bycinska@gmail.com>
2024-08-23 15:04:18 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const spiderLeft = (wall.clientWidth - spider.clientWidth) / 2;
+const spiderTop = (wall.clientHeight - spider.clientHeight) / 2;
+spider.style.left = `${spiderLeft}px`;
+spider.style.top = `${spiderTop}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
